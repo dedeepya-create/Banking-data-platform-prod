a68393e6ed665ace850b93e1508d7850068e3055
Dedeepya-create <mushroomred933@gmail.com>
2026-05-21 15:55:11 +0000

    branch-002

--- datagenerator/fetch_marketdata.py
+++
@@ -1,7 +0,0 @@
-import requests
-import pandas as pd
-from datetime import datetime
-import boto3
-import os
-import sys
-import time
--- tests/tests.py
+++ tests/tests.py
@@ -2,13 +2,13 @@
 import sys
 sys.path.insert(0, '.')
 from utils.constants import (
-    API_KEY,
-    STOCK_SYMBOLS,
-    FOREX_PAIRS,
-    AWS_BUCKET_NAME
+    
+    AWS_BUCKET_NAME,
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY
 )
 
-print(f"✅ API Key: {API_KEY[:8]}...")
-print(f"✅ Stocks: {STOCK_SYMBOLS}")
-print(f"✅ Forex: {FOREX_PAIRS}")
+
 print(f"✅ Bucket: {AWS_BUCKET_NAME}")
+print(f"✅ Forex: {AWS_ACCESS_KEY_ID}")
+print(f"✅ Bucket: {AWS_SECRET_ACCESS_KEY}")
